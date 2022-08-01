437e7b971bdf7a35d37351112dfededb77f80db7
Ankit <ankitpandey2812@gmail.com>
2022-08-02 01:34:45 +0530

    initial implementation

--- charging.cpp
+++ charging.cpp
@@ -3,6 +3,9 @@
 std::string getCurrentRangeOccurance(int* ptr)
 {
 	std::string result;
-	
+	if ((ptr[0] == ptr[1]) || (ptr[0] == (ptr[1] + 1)) || (ptr[0]) == (ptr[1] - 1))
+	{
+		result = std::to_string(ptr[0]) + "-" + std::to_string(ptr[1]) + "," + std::to_string(2);
+	}
 	return result;
 }

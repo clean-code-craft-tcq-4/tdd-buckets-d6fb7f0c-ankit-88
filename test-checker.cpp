05f7cbec5866fffcae21e59d4f502fc3ccee393c
Ankit <ankitpandey2812@gmail.com>
2022-08-02 01:53:41 +0530

    test folder

--- test-checker.cpp
+++ test-checker.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "catch.hpp"
+#include "test/catch.hpp"
 #include "charging.hpp"
 
 TEST_CASE("provide only 2 value in array") {

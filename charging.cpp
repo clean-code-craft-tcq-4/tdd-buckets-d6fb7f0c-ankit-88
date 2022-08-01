2a3e669e549980e8d3a7bcf573bf0fee7c094b62
Ankit <ankitpandey2812@gmail.com>
2022-08-02 01:32:03 +0530

    initial function change:

--- charging.cpp
+++ charging.cpp
@@ -1,1 +1,8 @@
+#include "charging.hpp"
 
+std::string getCurrentRangeOccurance(int* ptr)
+{
+	std::string result;
+	
+	return result;
+}
--- charging.hpp
+++ charging.hpp
@@ -1,1 +1,8 @@
+#ifndef CHARGING_HPP
+#define charging.hpp
+#include <iostream>
+#include<string>
 
+std::string getCurrentRangeOccurance(int* ptr);
+
+#endif
